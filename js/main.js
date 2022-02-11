cb4cd8e7319c31aa1f966f43f5e5ddad79c38701
angela <awander5324@gmail.com>
2022-02-11 13:58:14 +0800

    T6 V3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+// import Swiper bundle with all modules installed
+// import Swiper from 'swiper/bundle';
+// import styles bundle
+// import 'swiper/css/bundle';
+
 const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 4,
   spaceBetween: 25,
-  loop: true,
 
   breakpoints: {
     375: {
